1badcd91152d1fb9ee06ad93c1b1bb91569a4cd3
George K. Thiruvathukal <gkt@cs.luc.edu>
2013-08-09 19:48:00 -0500

    restoring the build scripts

--- deploy-gradhandbook.sh
+++ deploy-gradhandbook.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
-[ -f ~/.env/sphinx/bin/activate ] && . ~/.env/sphinx/bin/activate
 
-<<<<<<< local
-make -f makeDefault html
-make -f makeDefault latexpdf
-=======
 [ -f ~/.env/sphinx/bin/activate ] && . ~/.env/sphinx/bin/activate
 
 make clean
 make html
 make latexpdf
->>>>>>> other
 make epub
-<<<<<<< local
-cp CS-Grad-Internship-MOU.pdf build
-rsync -av -e "ssh -oPort=22222" $(pwd)/build/ gkt@gradhandbook.cs.luc.edu:/var/www/vhosts/gradhandbook.cs.luc.edu/htdocs/
-=======
+
+# I don't like having a PDF in the repository. But if we are going to keep
+# them here, please put them in pdf.in so the top-level directory remains
+# relatively uncluttered.
+
+cp pdf.in/* build
 
 ./rsync-gradhandbook.sh
-
-
->>>>>>> other
