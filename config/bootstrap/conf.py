1acdf245df53256fa12b521ffa7acd726677b006
Nicholas M. Synovic <Nicholas.Synovic@GMail.com>
2021-04-01 11:35:17 +0000

    Ran isort

--- config/bootstrap/conf.py
+++ config/bootstrap/conf.py
@@ -11,7 +11,9 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import os
+import sys
+
 import sphinx_bootstrap_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
