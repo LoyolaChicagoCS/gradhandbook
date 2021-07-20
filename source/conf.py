b274a930a314419251a444c9e7c5a441d96afc97
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-07-20 12:59:53 +0000

    Ran isort

--- source/conf.py
+++ source/conf.py
@@ -11,8 +11,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
-
+import os
+import sys
 from datetime import date
 
 version = date.today().strftime("Updated %Y-%m-%d")
