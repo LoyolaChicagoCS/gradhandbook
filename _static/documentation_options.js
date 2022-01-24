f256d34d0601589ca4a463f490767dd0b20cc596
chantin-luc <chantin-luc@users.noreply.github.com>
2022-01-24 17:58:13 +0000

    deploy: 65b4a82202a09225439e6e67b1b4950a3571d374

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Updated 2021-11-23',
+    VERSION: 'Updated 2022-01-24',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
